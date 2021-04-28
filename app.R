a09803a3f9d6a035802ec29f73b2ba1c5ab95a7b
Oddmund Huseby <huseby.oddmund@gmail.com>
2021-04-28 09:27:54 +0200

    Added new plot and removed unused code

--- app.R
+++ app.R
@@ -10,25 +10,39 @@ client <- fhirClient$new("http://hapi.fhir.org/baseR4")
 
 ui <- dashboardPage(
   dashboardHeader(title = "Basic dashboard"),
-  dashboardSidebar(),
-  dashboardBody(
-    # Boxes need to be put in a row (or column)
-    fluidRow(
-      column(width = 12,
-        box(plotOutput("plot1", height = 250)),
-        box(
-          title = "Controls",
-          selectInput("patientId", "Choose Patient", getPatientList(client)),
-        ),
-      )
+  dashboardSidebar(sidebarMenu(
+    menuItem(
+      "Browse Patients",
+      tabName = "browse",
+      icon = icon("dashboard")
     ),
-    fluidRow(
-      column(width = 12,
-        box(plotOutput("plot2", height = 250)),
-        box(plotOutput("plot3", height = 250)),
-      )
-    )
-  )
+    menuItem("All Patients", tabName = "all", icon = icon("th"))
+  )),
+  dashboardBody(tabItems(
+    ##First tab
+    tabItem(tabName = "browse",
+            # Boxes need to be put in a row (or column)
+            fluidRow(column(
+              width = 12,
+              box(plotOutput("plot1", height = 250)),
+              box(
+                title = "Controls",
+                selectInput("patientId", "Choose Patient", getPatientList(client)),
+              ),
+            )),
+            fluidRow(column(
+              width = 12,
+              box(plotOutput("plot2", height = 250)),
+              box(plotOutput("plot3", height = 250)),
+            ))),
+    
+    ## Second tab
+    tabItem(tabName = "all",
+            fluidRow(column(
+              width = 24,
+              box(plotOutput("plot4", height = 500)),
+            )))
+  ))
 )
 
 server <- function(input, output) {
@@ -85,6 +99,26 @@ server <- function(input, output) {
            title = paste("Body Temperature (for patient ", input$patientId, ")", sep =""))
      }
    })
+   
+   output$plot4 <- renderPlot({
+     patientData <- getPatientRespiatoryRateAll(client)
+     if (is.null(patientData)){
+       ggplot() +
+         theme_void() +
+         geom_text(aes(0,0,label=paste('No data for patientId', input$patientId, sep = " "))) +
+         xlab(NULL)
+     }else{
+       ggplot(data = allPatients, aes(x = as.Date(DATE), y = RESP, color=PATIENTID)) + 
+         geom_point() + 
+         geom_line() +
+         scale_x_date(
+           #limits = c(Sys.Date() - 200, NA),
+           date_labels = "%d-%m-%Y")+
+         labs(x = "Date", y = "Breaths per minute", title = "Respiratory rate")
+     }
+   })
+
 }
 
 shinyApp(ui, server)
+
